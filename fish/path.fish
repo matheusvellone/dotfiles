b071d6590ca786c6309c96324b18cd7d8f1441fb
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2018-05-10 21:46:32 -0300

    Directory structure and fish config file

---
+++ fish/path.fish
@@ -0,0 +1,1 @@
+set -U fish_prompt_pwd_dir_length 0
--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+mkdir -p ~/.config/fish
+echo "source $PWD/fish/main.fish" > /home/$USER/.config/fish/config.fish
+
 mkdir -p /home/$USER/.config/nvim
-ln -s $PWD/init.vim /home/$USER/.config/nvim/init.vim
+ln -s $PWD/vim/init.vim /home/$USER/.config/nvim/init.vim
 
 mkdir -p /home/$USER/.config/Code/User
-ln -s $PWD/vscode.json /home/$USER/.config/Code/User/settings.json
+ln -s $PWD/vscode/vscode.json /home/$USER/.config/Code/User/settings.json
