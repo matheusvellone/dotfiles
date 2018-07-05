0aefc1243331bcd8f284188d421d1edc9ba8fe10
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2018-07-05 17:27:23 -0300

    fish: add git-clean-branch command

--- fish/functions.fish
+++ fish/functions.fish
@@ -1,3 +1,8 @@
 function mkcd
     mkdir $argv; and cd $argv
 end
+
+function git-clean-branch
+    git remote prune origin
+    git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D
+end
