387aae9af11564abf29aedd1d8fb8925b27df874
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2018-07-10 13:45:54 -0300

    fish: add delete git tag function

--- fish/functions.fish
+++ fish/functions.fish
@@ -6,3 +6,8 @@ function git-clean-branch
     git remote prune origin
     git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D
 end
+
+function git-delete-tag
+    git tag -d $argv
+    git push origin :refs/tags/$argv
+end
