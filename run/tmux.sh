73f1e9e9979a536c971aab0faaedc62e0b60a453
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2018-09-20 12:07:35 -0300

    tmux: add tmux plugin manager (TPM)

--- run/tmux.sh
+++ run/tmux.sh
@@ -1,3 +1,10 @@
 echo "Running tmux configuration"
 
-ln -s $PWD/tmux/.tmux.conf /home/$USER/.tmux.conf
+TPM_DIR=~/.tmux/plugins/tpm
+
+if [ ! -d "$TPM_DIR" ]; then
+  echo "Cloning TPM plugin into $TPM_DIR"
+  git clone --quiet https://github.com/tmux-plugins/tpm $TPM_DIR
+fi
+
+ln -fs $PWD/tmux/.tmux.conf /home/$USER/.tmux.conf
