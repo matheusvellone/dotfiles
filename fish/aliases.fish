01a55a9296fe95670c71f4e3b1c343ba52486318
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2018-09-23 22:42:25 -0300

    fish: add vim -> nvim alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,1 +1,2 @@
+alias vim "nvim"
 alias pagarme-vpn "sudo openvpn --config ~/Documents/pagarme/openvpn/config.ovpn --ca ~/Documents/pagarme/openvpn/ca.crt --cert ~/Documents/pagarme/openvpn/client.crt --key ~/Documents/pagarme/openvpn/client.key"
