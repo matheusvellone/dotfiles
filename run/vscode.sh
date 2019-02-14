8f5d68261181eae474bafce18bca355caa8461d9
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2019-02-14 21:56:20 -0200

    vscode: add snippets

--- run/vscode.sh
+++ run/vscode.sh
@@ -2,4 +2,4 @@ echo "Running vscode dotfile configuration"
 
 mkdir -p /home/$USER/.config/Code/User
 ln -fs $PWD/vscode/vscode.json /home/$USER/.config/Code/User/settings.json
-ln -fs $PWD/vscode/keybindings.json /home/$USER/.config/Code/User/keybindings.json
+ln -fsd $PWD/vscode/snippets /home/$USER/.config/Code/User
