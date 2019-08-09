53f804e8f5c6e8f9758edfb06672dcfbc9bb11e2
Matheus Peviani Vellone <matheusvellone@gmail.com>
2019-08-09 00:32:28 -0300

    vscode: add keybindings to run command and fix snippets

--- run/vscode.sh
+++ run/vscode.sh
@@ -2,4 +2,7 @@ echo "Running vscode dotfile configuration"
 
 mkdir -p $HOME/.config/Code/User
 ln -fs $PWD/vscode/vscode.json $HOME/.config/Code/User/settings.json
+ln -fs $PWD/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
+
+rm -rf $HOME/.config/Code/User/snippets
 ln -fsd $PWD/vscode/snippets $HOME/.config/Code/User
