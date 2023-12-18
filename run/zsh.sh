76facea84fca6cdfa3c7672b5c6546fc02a40ad4
Matheus Peviani Vellone <matheusvellone@gmail.com>
2023-12-18 20:48:38 +0000

    add bat and lsd

--- run/zsh.sh
+++ run/zsh.sh
@@ -5,6 +5,8 @@ sudo snap install bw
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
 ln -fs $PWD/zsh/.zshrc $HOME/.zshrc
 ln -fs $PWD/zsh/.p10k.zsh $HOME/.p10k.zsh
 
@@ -12,3 +14,9 @@ if ! [ -x "$(command -v fzf)" ]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
 fi
+
+sudo apt install bat -y
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+# lsd
+sudo apt install lsd -y
